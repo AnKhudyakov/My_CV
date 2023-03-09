c1af6b16c9a68ade051a3414cfdb542b9b8438c6
AnKhudyakov <andrew_job@list.ru>
2023-03-09 20:37:32 +0400

    fix routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import HomeView from '../views/HomeView.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
@@ -20,7 +20,7 @@ const routes = [
 
 const router = createRouter({
   routes,
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
 });
 
 export default router;
